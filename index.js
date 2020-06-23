e9546b645e704ac5897708b5e244f429b7ca3660
oxy <elektronikaion@gmail.com>
2020-06-23 17:32:48 +0300

    Config

--- index.js
+++ index.js
@@ -11,13 +11,23 @@ const app = express();
 
 const db = mongoose.connection;
 
+const isProduction = process.env.NODE_ENV === "production";
+
 app.use(morgan("tiny"));
 app.use(bodyParser.json());
+
+// * Database connection
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-m2kce.mongodb.net/${process.env.DB_COLLECTION}?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
 );
+
+if (!isProduction) {
+  mongoose.set("debug", true);
+}
+
+// * Headers config
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -29,19 +39,55 @@ app.use((req, res, next) => {
   next();
 });
 
+// * Router
+
 app.use("/posts", postsRouter);
 app.use("/auth", authRouter);
 
-app.use((error, req, res, next) => {
-  console.error(error);
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-  res.status(status).json({ message, data });
+// * Error handlers
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+// * Launch
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
   console.log("Database connection success");
-  app.listen(process.env.PORT || 8081);
+  const server = app.listen(process.env.PORT || 8081, function () {
+    console.log("Listening on port " + server.address().port);
+  });
 });
